f47ca5cc0343b67a198c43a8e3cf16873154b545
teshigawara3 <kazuhisa246@gmail.com>
2020-10-23 07:53:54 +0900

    Update twitter_search.rb

--- twitter_search.rb
+++ twitter_search.rb
@@ -5,10 +5,10 @@ require "twitter"
 #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key       = "F4CePTFDSzzNPWy1gtroWt0uG"
-  config.consumer_secret    = "ujQjfMYDiAreIT9z3m1JKmci1s0FgIDKwo8pJzoRpEqw1g2s0U"
-  config.access_token        = "1407219055-1ZlToB4NmTRs8Wcl9FUZfbH6sWJMFKNhaVV3FPD"
-  config.access_token_secret = "olA7RsVeSOjXO1Z6ULBodJaI6OhsLMAu0FNm4nIkn7x0l"
+  config.consumer_key       = "XXXXXXXXXXXXXXXXXXXX"
+  config.consumer_secret    = "XXXXXXXXXXXXXXXXXXXX"
+  config.access_token        = "XXXXXXXXXXXXXXXXXXXX"
+  config.access_token_secret = "XXXXXXXXXXXXXXXXXXXX"
 end
 
 msList = ScrapingMovie.new.GetMovies()
